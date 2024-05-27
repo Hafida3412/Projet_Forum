0d89900295474be0e51780848d2aab79638aaaf6
Hafida 3412 <lounsy81@gmail.com>
2024-05-27 12:01:09 +0200

    afficher message

---
+++ view/forum/listMessages.php
@@ -0,0 +1,11 @@
+<?php
+    $sujet = $result["data"]['sujet']; 
+    $messages = $result["data"]['messages']; 
+?>
+
+<h1>Liste des messages</h1>
+
+<?php
+foreach($messages as $message ){ ?>
+    <p><a href="index.php?ctrl=forum&action=MessagesBySujet&id=<?= $sujet->getId() ?>"<?= $sujet->getId() ?>><?= $sujet ?></a> par <?= $sujet->getUtilisateur() ?> (<?= $sujet->getDateCreationSujet() ?>)<?= $message->getTexte() ?></p>
+<?php }
