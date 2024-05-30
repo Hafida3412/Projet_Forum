019a82051d1293deb970e8f9638cef2b921d7974
Hafida 3412 <lounsy81@gmail.com>
2024-05-30 14:55:34 +0200

    créations des vues login et register

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -5,9 +5,12 @@ use App\AbstractController;
 use App\ControllerInterface;
 
 class SecurityController extends AbstractController{
+    // Affiche la vue du formulaire register
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
+    public function register () {}       
+
     public function login () {}
     public function logout () {}
+//}
 }
